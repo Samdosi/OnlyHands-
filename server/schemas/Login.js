01478e5fba3fcd63d5dca2ccbab637006e491e9f
Joseph <josephmt1176@gmail.com>
2022-10-19 20:08:47 -0400

    server - schemas - updated Login and Profile

--- server/schemas/Login.js
+++ server/schemas/Login.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
 const LoginSchema = new Schema({
     username: {
@@ -17,24 +15,8 @@ const LoginSchema = new Schema({
         type: String,
         required: true,
         unique: true
-    },
-    gender: {
-        type: String,
-        required: true
     }
 });
-
-LoginSchema.pre('save', async function (next) {
-    try {
-        const hashedPassword = await bcrypt.hash(this.password, saltRounds);
-        this.password = hashedPassword;
-        next();
-    } catch (err) {
-        next(err);
-    }
-});
-
-
 
 const Login = new mongoose.model('login', LoginSchema);
 module.exports = Login;
--- server/schemas/Profile.js
+++ server/schemas/Profile.js
@@ -10,6 +10,10 @@ const profileSchema = new Schema({
         type: String,
         required: true
     },
+    gender: {
+        type: String,
+        required: true
+    },
     age: {
         type: Number,
         required: true
