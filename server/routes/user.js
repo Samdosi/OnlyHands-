a819d7a84923c48d56a08b4cebe556b310c4df2c
Joseph <josephmt1176@gmail.com>
2022-10-19 20:10:35 -0400

    server - controllers - added login and createUser fixed created user and added JWT auth fixed login and added authorization

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,25 +1,34 @@
 const express = require('express');
-const { register } = require('../controllers/user');
+const { createUser, login } = require('../controllers/user');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 
 router.get('/');
 
 router.post('/', async (req, res) => {
-    //TODO: encrypt password
+
     //TODO: validate body
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    const gender = req.body.gender;
 
-
-    await register({
+    await createUser({
         username,
         password,
-        email,
-        gender
+        email
     }, res);
+});
+
+router.post('/login', async (req, res) => {
+
+    //TODO: Validate
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await login({
+        password,
+        username
+    }, res)
 });
 
 module.exports = router;
