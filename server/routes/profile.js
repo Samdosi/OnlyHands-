cd58aff025698f8e60b9a938ad4e0dcf27e7459a
rexbianchi <rexbianchi@protonmail.com>
2022-10-24 19:33:27 -0400

    Create profile endpoint

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth_jwt = require("../util/auth_jwt");
+const { create_profile } = require("../controllers/profile");
 
 
 router.get("/", auth_jwt, (req, res) => {
@@ -8,9 +9,8 @@ router.get("/", auth_jwt, (req, res) => {
 
 });
 
-router.post("/", auth_jwt, (req, res) => {
-
-
+router.post("/", auth_jwt, async (req, res) => {
+    await create_profile(req, res);
 });
 
 router.put("/", auth_jwt, (req, res) => {
