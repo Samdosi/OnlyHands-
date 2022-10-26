ea6e79d080207423210d0eb38ef4d52da82cd4e3
DaniOstos <101365912+DaniOstos@users.noreply.github.com>
2022-10-26 19:48:21 -0400

    Fixed user schema import

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,4 @@
-const User = require("../schemas/User");
+const {User} = require("../schemas/User");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
