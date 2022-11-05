6f6b91ee2d735c761d70ad489a18ebd7f7f48391
James Nguyen <70236137+HJaen@users.noreply.github.com>
2022-11-05 11:51:01 -0400

    Fixed production deployment code

--- index.js
+++ index.js
@@ -25,15 +25,12 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// From Leinecker
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') 
-{
+// For product deployment
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../client/build'));
-  app.get('*', (req, res) => 
- {
-    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
