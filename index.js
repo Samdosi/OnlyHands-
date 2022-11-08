d03315ef3989568f46358068ed2a3ab7dcaac01f
Joseph <josephmt1176@gmail.com>
2022-11-07 19:41:02 -0500

    moved path connection to front end to button

--- index.js
+++ index.js
@@ -25,17 +25,8 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// For product deployment
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 const PORT = process.env.PORT || 5000;
-
 
 app.use('/api/user', userRoute);
 app.use('/api/profile', profile_route);
@@ -64,13 +55,22 @@ mongoose.connect(
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
-);
-
+  );
+  
 const db = mongoose.connection;
 db.once("open", () => {
   console.log("Connected successfully");
 });
 db.on("error", console.error.bind(console, "connection error: "));
-
+  
+  
+// For product deployment
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 serverChat.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
