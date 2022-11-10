6c67ce64a2e991dd5454f27e259fc3ddee02a3a7
Rex Bianchi <rexbianchi@protonmail.com>
2022-11-10 00:29:37 -0500

    Image upload functionality

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -1,5 +1,6 @@
 const { Profile } = require("../schemas/Profile");
 const { User } = require("../schemas/User");
+const { ImageUpload } = require("../schemas/ImageUpload");
 
 
 
@@ -25,7 +26,6 @@ const create_profile = async (req, res) => {
 
             found_user.profile = user_profile;
             found_user.save((err) => {
-
                 return res.status(200).json({ "message": "User Profile Created!" });
             });
         });
@@ -48,6 +48,11 @@ const get_profile = async (req, res) => {
     } else {
         return res.status(400).json("Profile ID is required");
     }
+};
+
+const get_own_profile = async (req, res) => {
+
+
 };
 
 const edit_profile = async (req, res) => {
@@ -81,5 +86,51 @@ const edit_profile = async (req, res) => {
     });
 };
 
+const upload_image = async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.status(400).json({ "success": false, "message": "No file provided!" });
+        }
 
-module.exports = { create_profile, get_profile, edit_profile };
+        User.findById(req.user_id, (err, found_user) => {
+            if (err || !found_user) {
+                return res.status(400).json({ "success": false, "message": "User not found!" });
+            }
+
+            Profile.findById(found_user.profile, (err, found_profile) => {
+                if (err || !found_profile) {
+                    return res.status(400).json({ "success": false, "message": "Profile does not exist!" });
+                }
+
+                const profile_image = new ImageUpload({
+                    fileName: req.body.fileName,
+                    file: {
+                        data: req.file.buffer,
+                        contentType: req.file.mimetype
+                    }
+                });
+
+                profile_image.save(err => {
+                    if (err) {
+                        return res.status(400).json({ "success": false, "message": "Database error!" });
+                    }
+
+                    found_profile.save(err => {
+                        if (err) {
+                            return res.status(400).json({ "success": false, "message": "Database error!" });
+                        }
+
+                        return res.status(200).json({ "success": true, "image": "Image Uploaded Successful!" });
+                    })
+                });
+            });
+        });
+
+
+    } catch (err) {
+        return res.status(500).json({ "success": false, "message": "Server Error!" });
+    }
+};
+
+
+module.exports = { create_profile, get_profile, edit_profile, upload_image };
