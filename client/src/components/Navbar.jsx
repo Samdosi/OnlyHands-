da73119ec9cff9693862197ff7aff07763761d93
Joseph <josephmt1176@gmail.com>
2022-11-10 18:41:45 -0500

    fixed hamburger position in Navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -61,8 +61,8 @@ const Navbar = () => {
         <u className='hidden md:flex justify-between lg:justify-around text-white w-1/3 no-underline list-none font-medium '>
           {links}
         </u>
-        <div ref={hamburgerRef} className='absolute top-1/4 right-0 md:hidden'>
-          <div className='z-10 absolute top-0 right-0'>
+        <div ref={hamburgerRef} className=' z-10 absolute top-1/2 translate-y-[-50%] right-0 md:hidden'>
+          <div className='z-10 relative top-0 right-0'>
             <Hamburger
               color={toggleHamburger ? 'black' : 'white'}
               toggled={toggleHamburger}
