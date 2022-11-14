ce3d5a9f66b68f00f161edbe339b1bb4f9875b1b
JosefVodicka <josef.vodicka@yahoo.com>
2022-11-14 18:12:57 -0500

    Updated users

--- assets/data/usersTest.js
+++ assets/data/usersTest.js
@@ -1,23 +1,23 @@
 export default [
     {
       id: '1',
-      name: 'Vadim Savin',
+      name: 'Conor McGregor',
       image:
-        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/vadim1.JPG',
+        'https://media.gettyimages.com/id/839485722/photo/floyd-mayweather-jr-v-conor-mcgregor-weigh-in.jpg?s=2048x2048&w=gi&k=20&c=ZFe9-YdUY0AVvPAL19PNr06eeKaQNkU1lMdAgSyRr78=',
       bio: 'I will be the semicolons to your code',
     },
     {
       id: '2',
-      name: 'Elon Musk',
+      name: 'Floyd Mayweather',
       image:
-        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png',
+        'https://media.gettyimages.com/id/487863041/photo/floyd-mayweather-jr-v-marcos-maidana-weigh-in.jpg?s=1024x1024&w=gi&k=20&c=HvEsm-x34ACQM1zYp8Qf0NG6oMAqk8_9gWqMc4jO0YU=',
       bio: 'A dude with a rocket is looking for a gal with fuel',
     },
     {
       id: '3',
-      name: 'Zuck',
+      name: 'Jake Paul',
       image:
-        'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg',
+        'https://media.gettyimages.com/id/1203109888/photo/jake-paul-v-anesongib.jpg?s=1024x1024&w=gi&k=20&c=9M-qNHg8R3ihGtw5NiXUjKU2AavUzfzlxVAmaOtwui8=',
       bio: 'No need to send me your nudes, I already saw them',
     },
     {
