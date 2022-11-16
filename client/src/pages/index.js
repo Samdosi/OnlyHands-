286e2aa1aaf4b0268d3ba30e18c3b24717e31cbf
jadynLP <jadynlapace@gmail.com>
2022-11-16 18:52:46 -0500

    More profile stuff

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,3 +1,3 @@
 export {default as Hero} from './Hero/Hero.jsx';
 export {default as Chat} from './Chat/Chat.jsx';
-export {default as Profile} from './Profile/Profile.jsx';
+// export {default as Profile} from './Profile/Profile.jsx';
