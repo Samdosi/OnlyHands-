3e354cbfaf8298eff4d2352277a7bb4dbfe73fe2
rexbianchi <78284807+rexbianchi@users.noreply.github.com>
2022-11-21 18:17:30 -0500

    Added getUser to profile route

--- routes/profile.js
+++ routes/profile.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { auth_jwt } = require("../middleware/auth_jwt");
 const { create_profile, get_profile, edit_profile, upload_image } = require("../controllers/profile");
 const { validateInput } = require("../middleware/validateInput");
+const { getUser } = require("../controllers/user");
 
 const multer = require('multer')
 const storage = multer.memoryStorage();
@@ -11,7 +12,7 @@ const upload = multer({ storage: storage });
 
 router.get("/", auth_jwt, async (req, res) => {
     if (!req.body.user_req.profile_id) {
-        const user = await get_user(req.body.user_id);
+        const user = await getUser(req.body.user_id);
         await get_profile(user.profile_id, res);
     }
     else {
