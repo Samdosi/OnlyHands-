4a37920ca5e129c891ffa5513c8e4f957e8b501a
Samdosi <87053736+Samdosi@users.noreply.github.com>
2022-12-01 20:11:31 -0500

    Updated

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,54 +1,54 @@
-import React,{useEffect, useState} from "react";
-import 'react-native-gesture-handler';
+import React, { useEffect, useState } from "react";
+import "react-native-gesture-handler";
 
-import 
-{ Text,
-    StyleSheet,
-    ImageBackground, 
-    Image,
-    SafeAreaView,
-    View, 
-    Button, 
-    Pressable, 
-    useWindowDimensions,
-    Alert,
-    
+import {
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  SafeAreaView,
+  View,
+  Button,
+  Pressable,
+  useWindowDimensions,
+  Alert,
 } from "react-native";
-import Card from '../src/components/card'
-import users from '../assets/data/usersTest'
-import Animated,
-{   useSharedValue,
-    useAnimatedStyle,
-    withSpring,
-    useAnimatedGestureHandler,
-    useDerivedValue,
-    interpolate,
-    runOnJS,
-    color,
-    
-} from 'react-native-reanimated'
+import Card from "../src/components/card";
+import users from "../assets/data/usersTest";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withSpring,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  interpolate,
+  runOnJS,
+  color,
+} from "react-native-reanimated";
 
-import {PanGestureHandler, TouchableOpacity,TapGestureHandler} from 'react-native-gesture-handler'
-import Fight from '../assets/fight.png'
-import NoFight from '../assets/no!.png'
-
+import {
+  PanGestureHandler,
+  TouchableOpacity,
+  TapGestureHandler,
+} from "react-native-gesture-handler";
+import Fight from "../assets/fight.png";
+import NoFight from "../assets/no!.png";
+import profileEdit from "./profileEdit";
 const ROTATION = 60;
 const SWIPE_VELOCITY = 1000;
 
-import AntDesign from 'react-native-vector-icons/AntDesign'
-import Entypo from 'react-native-vector-icons/Entypo'
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
+import AntDesign from "react-native-vector-icons/AntDesign";
+import Entypo from "react-native-vector-icons/Entypo";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { BackgroundImage } from "react-native-elements/dist/config";
 import axios from "axios";
-import load from '../loadprofiles'
+import load from "../loadprofiles";
+import ChatScreen from "./ChatScreen";
 
+const Home = ({ navigation, route }) => {
+  //const profiles = load(route.params.paramKey)
 
-const Home = ({navigation,route}) => {
-
-    //const profiles = load(route.params.paramKey)
-    
-
- /*   const LoadProfiles = async () => {
+  /*   const LoadProfiles = async () => {
         const token = route.params.paramKey;
         console.log(token)
        //console.log('HEY');
@@ -77,118 +77,100 @@ const Home = ({navigation,route}) => {
             
     }*/
 
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const [nextIndex, setNextIndex] = useState(currentIndex+1)
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [nextIndex, setNextIndex] = useState(currentIndex + 1);
 
-    const currentProfile = users[currentIndex];
-    const nextProfile = users[nextIndex];
+  const currentProfile = users[currentIndex];
+  const nextProfile = users[nextIndex];
 
-    const {width: screenWidth} = useWindowDimensions();
+  const { width: screenWidth } = useWindowDimensions();
 
-    const hiddenTranslateX = 2 * screenWidth
+  const hiddenTranslateX = 2 * screenWidth;
 
-    const translateX = useSharedValue(0);
-    const rotate = useDerivedValue(()=> interpolate(
-        translateX.value,
-        [0, hiddenTranslateX],
-        [0, ROTATION],        
-    )+ 'deg',
-    );
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
+      "deg"
+  );
 
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      },
+    ],
+  }));
 
+  const nextCardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        scale: interpolate(
+          translateX.value,
+          [-hiddenTranslateX, 0, hiddenTranslateX],
+          [1, 0.8, 1]
+        ),
+      },
+    ],
 
-    const cardStyle = useAnimatedStyle(() => ({
-        transform:[
-        {
-            translateX: translateX.value,
-        },
-        {
-            rotate: rotate.value,
-        }
-        ],
-    }))
+    opacity: interpolate(
+      translateX.value,
+      [-hiddenTranslateX, 0, hiddenTranslateX],
+      [1, 0.5, 1]
+    ),
+  }));
 
-    const nextCardStyle = useAnimatedStyle(() => ({
-        transform:[
-            {scale:  interpolate(
-                    translateX.value,
-                    [-hiddenTranslateX,0, hiddenTranslateX],
-                    [1, 0.8, 1],        
-                    ),
-            }
-        ],
+  const figtStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, hiddenTranslateX / 5], [0, 1]),
+  }));
 
-        opacity:interpolate(
-            translateX.value,
-            [-hiddenTranslateX,0, hiddenTranslateX],
-            [1, 0.5, 1],        
-            ),
-    }))
+  const noFightStyle = useAnimatedStyle(() => ({
+    opacity: interpolate(translateX.value, [0, -hiddenTranslateX / 5], [0, 1]),
+  }));
 
-    const figtStyle = useAnimatedStyle(()=>({
-        opacity:interpolate(
-            translateX.value,
-            [0, hiddenTranslateX/5],
-            [0, 1],        
-            ),
-    }))
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
 
-    const noFightStyle = useAnimatedStyle(()=>({
-        opacity:interpolate(
-            translateX.value,
-            [0, -hiddenTranslateX/5],
-            [0, 1],        
-            ),
-    }))
+      // console.warn('Touch Start');
+    },
 
-    const gestureHandler = useAnimatedGestureHandler({
-        onStart:(_, context) =>{
-            context.startX = translateX.value;
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
 
+      //console.log(event.velocityX)
+    },
 
-           // console.warn('Touch Start');
-        },
+    onEnd: (event) => {
+      if (Math.abs(event.velocityX) < SWIPE_VELOCITY) {
+        //console.log(event.velocityX)
+        translateX.value = withSpring(0);
 
-        onActive:(event, context)=>{
-            translateX.value = context.startX + event.translationX;
+        return;
+      }
 
+      translateX.value = withSpring(
+        hiddenTranslateX * Math.sign(event.velocityX),
+        {},
+        () => runOnJS(setCurrentIndex)(currentIndex + 1)
+      );
+      //post match
+    },
+  });
 
-            //console.log(event.velocityX)   
-        },
-
-        onEnd:(event)=>{
-             
-
-            if(Math.abs(event.velocityX) < SWIPE_VELOCITY){
-                //console.log(event.velocityX)  
-                translateX.value = withSpring(0);
-
-                return;
-            }
-           
-            translateX.value = withSpring(
-                hiddenTranslateX * Math.sign(event.velocityX),
-                {},
-                () => runOnJS(setCurrentIndex)(currentIndex+1)
-                );
-                //post match          
-        }
-    });
-
-
-
-    useEffect(() => {
-        translateX.value = 0;
-        setNextIndex(currentIndex + 1);
-        
-    },[currentIndex,translateX])
+  useEffect(() => {
+    translateX.value = 0;
+    setNextIndex(currentIndex + 1);
+  }, [currentIndex, translateX]);
 
   //  users[0].name = 'Sam'
-//
+  //
 
-
-//IF YES or NO buttons, but F that
-    /*
+  //IF YES or NO buttons, but F that
+  /*
                 <View style = {styles.botNavigation}>
                     
                     <View style = {[styles.botButton,{backgroundColor:'#ff6969'}]}>
@@ -229,180 +211,176 @@ const Home = ({navigation,route}) => {
     }
 */
 
-    return(
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#e6e6e3" }}>
+      <View style={{ alignItems: "center", justifyContent: "center", top: 60 }}>
+        <Image
+          style={styles.logoStyle}
+          source={require("../assets/logoTrans.png")}
+          resizeMode="contain"
+        ></Image>
+      </View>
 
-            <SafeAreaView style={{flex:1,backgroundColor:'#e6e6e3'}}>
-               
-                <View style={{alignItems:'center',justifyContent:'center',top:60}}>
-                    <Image  style={styles.logoStyle} source={require('../assets/logoTrans.png')}resizeMode='contain' ></Image>
-                </View>
-                
-                
-                <View  style={styles.pageContainer}>                
-                    <View style={styles.topNavigation}>
-                        <TouchableOpacity >
-                        <FontAwesome name="user-circle-o" size={40} color={'black'}/>
-                        </TouchableOpacity>
-                        <Entypo name="chat" size={40} color={'black'}/>
-                    </View>
+      <View style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate("profileEdit", {
+                paramKey: route.params.paramKey,
+              })
+            }
+          >
+            <FontAwesome name="user-circle-o" size={40} color={"black"} />
+          </TouchableOpacity>
+          <Entypo
+            name="chat"
+            size={40}
+            color={"black"}
+            onPress={() => navigation.navigate("ChatScreen")}
+          />
+        </View>
 
-                    
-                
-                  
+        {nextProfile && (
+          <View style={styles.nextCardContainer}>
+            <Animated.View style={[styles.animatedCard, nextCardStyle]}>
+              <Card user={nextProfile} />
+            </Animated.View>
+          </View>
+        )}
 
-                {nextProfile && (
-                    <View style = {styles.nextCardContainer}>
-                        <Animated.View style={[styles.animatedCard,nextCardStyle]}>
-                            <Card user = {nextProfile}/>
-                        </Animated.View>
-                    
-                    </View>
-               )}               
-               
-                {currentProfile && (
-                    <PanGestureHandler onGestureEvent={gestureHandler}>
-                    
-                        <Animated.View style={[styles.animatedCard,cardStyle]}>
-                            <Animated.Image source={Fight} style={[styles.fight,{left:10},figtStyle]} resizeMode='contain'></Animated.Image>
-                            <Animated.Image source={NoFight} style={[styles.fight,{right:10},noFightStyle]}></Animated.Image>
-                            <Card user = {currentProfile}/>
-                        </Animated.View>
-
-                                          
-                    </PanGestureHandler>
-                    
-                    
-                )}
-                <Button title="Click" style={{width:30,height:30,backgroundColor:'red'}} onPress = {load}>
-
-                </Button>
-
-                </View>         
-            </SafeAreaView>
-        
-    )
-}
+        {currentProfile && (
+          <PanGestureHandler onGestureEvent={gestureHandler}>
+            <Animated.View style={[styles.animatedCard, cardStyle]}>
+              <Animated.Image
+                source={Fight}
+                style={[styles.fight, { left: 10 }, figtStyle]}
+                resizeMode="contain"
+              ></Animated.Image>
+              <Animated.Image
+                source={NoFight}
+                style={[styles.fight, { right: 10 }, noFightStyle]}
+              ></Animated.Image>
+              <Card user={currentProfile} />
+            </Animated.View>
+          </PanGestureHandler>
+        )}
+        <Button
+          title="Click"
+          style={{ width: 30, height: 30, backgroundColor: "red" }}
+          onPress={load}
+        ></Button>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
+  bg: {
+    position: "absolute",
+  },
 
-    bg:{
-        position:'absolute'
+  logoStyle: {
+    position: "absolute",
+    //backgroundColor:'red',
+    height: 150,
+    width: 80,
+    //flexDirection:'column',
+
+    flex: 1,
+  },
+
+  botButton: {
+    height: 50,
+    width: 50,
+
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "50%",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
-    logoStyle:{
-        position:'absolute',
-        //backgroundColor:'red',
-        height:150,
-        width:80,
-        //flexDirection:'column',
-        
-        flex:1
+    elevation: 5,
+    borderWidth: 0.2,
+    borderColor: "#8d97a1",
+  },
+
+  botNavigation: {
+    width: "100%",
+    top: 50,
+    height: 50,
+    //position:'relative',
+    //paddingTop:50,
+    //backgroundColor:'red',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+
+  topNavigation: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "100%",
+    alignSelf: "stretch",
+    //backgroundColor:'blue',
+    bottom: 90,
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
 
-    botButton:{
-        height:50,
-        width:50,
-        
-        justifyContent:'center',
-        alignItems:'center',
-        borderRadius:'50%',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+    elevation: 5,
+  },
 
-        elevation: 5,
-        borderWidth:0.2,
-        borderColor:'#8d97a1'
-    },
+  innerPicture: {
+    width: 30,
+    height: 30,
+    alignSelf: "center",
+    opacity: 0.5,
+  },
 
-    botNavigation:{
-        width:'100%',
-        top:50,
-        height:50,
-        //position:'relative',
-        //paddingTop:50,
-        //backgroundColor:'red',
-        flexDirection:'row',
-        justifyContent:'space-around',
-        alignItems:'center'
-    },
+  fight: {
+    width: 150,
+    height: 150,
+    position: "absolute",
+    top: 10,
+    zIndex: 1,
+    elevation: 1,
+  },
 
-    topNavigation:{
-        
-        flexDirection:'row',
-        justifyContent:'space-between',
-        width:'100%',
-        alignSelf:'stretch',
-        //backgroundColor:'blue',
-        bottom:90,
-        paddingLeft:15,
-        paddingRight:15,
-        paddingTop:10,
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.84,
+  pageContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    //backgroundColor:'red'
+  },
 
-        elevation: 5,
-        
-        
-    },
+  animatedCard: {
+    width: "90%",
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    innerPicture:{
-        width:30,
-        height:30,
-        alignSelf:'center',
-        opacity:0.5
-        
-    },  
+  nextCardContainer: {
+    height: "100%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    flex: 1,
+    paddingTop: 15,
+  },
+});
 
-    
-
-    fight:{
-        width: 150,
-        height: 150,
-        position:'absolute',
-        top:10,
-        zIndex:1,
-        elevation:1,
-    },
-
-
-    pageContainer:{
-        justifyContent:'center',
-        alignItems:'center',
-        flex:1,
-        //backgroundColor:'red'
-    },
-
-    animatedCard:{
-        width: '90%',
-        height: '70%',
-        justifyContent:'center',
-        alignItems:'center',
-              
-    },
-
-    nextCardContainer:{
-        height:'100%',
-        width:'100%',        
-        justifyContent:'center',
-        alignItems:'center',  
-        position:'absolute',            
-        flex:1,
-        paddingTop:15
-        
-    },
-
-   
-
-})
-
-export default Home
+export default Home;
