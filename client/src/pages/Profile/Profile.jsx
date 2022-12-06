3a80480a3e5f4edbe9f3470087bf47335b894364
James Nguyen <hjaenscode@gmail.com>
2022-12-06 16:20:31 -0500

    Style changes and added avatars
    Co-authored-by: Jadyn Lapace <jadynlapace@Knights.ucf.edu>
    Co-authored-by: Leith  <Latharius@users.noreply.github.com>

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -2,17 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Card from './components/Card';
 import InfoModal from './components/InfoModal';
+import AvatarModal from './components/AvatarModal';
 import Cookies from "universal-cookie";
+import { CgProfile } from 'react-icons/cg';
+import { IconContext } from "react-icons";
+import imageOne from './avatars/1.jpg'
+import imageTwo from './avatars/2.jpg'
+import imageThree from './avatars/3.jpg'
+import imageFour from './avatars/4.jpg'
+import imageFive from './avatars/5.jpg'
 
 const Profile = ({ setBgImage }) => {
     const cookies = new Cookies();
     const { pathname } = useLocation();
     setBgImage(pathname);
-    const user = cookies.get("profile").firstName;
-
 
     const [showModal, setShowModal] = useState(false);
     const handleModal = () => setShowModal(false);
+    
+    const [avatar, setAvatar] = useState(null);
+    const [showAvatarModal, setShowAvatarModal] = useState(false);
+    const handleAvatarModal = () => setShowAvatarModal(false);
+
+
+    const avatarImages = [
+        imageOne,
+        imageTwo,
+        imageThree,
+        imageFour,
+        imageFive
+      ]
 
     useEffect(() => {
         fetch('https://only-hands.herokuapp.com/api/profile/', {
@@ -26,6 +45,7 @@ const Profile = ({ setBgImage }) => {
                 if (data["success"]) {
                     console.log(data["profile"])
                     cookies.set("profile", JSON.stringify(data["profile"]));
+                    setAvatar(data["profile"]["image"])
                 }
                 else {
                     console.log(data["message"]);
@@ -37,19 +57,33 @@ const Profile = ({ setBgImage }) => {
 
     return (
         <div>
-            <div classname="left col-span-1 bg-white">
+            <div className='flex flex-row justify-center center-items lg:justify-start'>
                 <button
                     onClick={() => { setShowModal(true) }}
                     className="bg-white transition text-black font-bold p-3 2xl:p-4 rounded-lg shadow-md grow-transition">
                     My Stats
                 </button>
+                <button
+                    onClick={() => { setShowAvatarModal(true) }}
+                    >
+                    {avatar ? 
+                    <img
+                        className='rounded-lg transition grow-transition ml-3'
+                        src={avatarImages[cookies.get("profile")["image"]]} 
+                        style={{width: "4em", height: "4em", borderRadius: "50%"}}
+                        />: 
+                    (<IconContext.Provider value={{ color: "white", size: "4em", className: "avatar_icon"}}>
+                        <CgProfile />
+                    </IconContext.Provider>)}
+                </button>
             </div>
             <div className="right col-span-2 flex flex-col justify-center items-center">
-                <div className="w-full relative flex flex-col justify-center overflow-hidden">
+                <div className="w-full flex flex-col justify-center overflow-hidden">
                     <Card />
                 </div>
             </div>
             {showModal && <InfoModal onClose={handleModal} />}
+            {showAvatarModal && <AvatarModal onClose={handleAvatarModal} />}
         </div>
     )
 }
--- client/src/pages/Profile/components/Card.jsx
+++ client/src/pages/Profile/components/Card.jsx
@@ -1,6 +1,15 @@
+// dark gray bg: #323232
+
 import React, { useState, useMemo, useRef, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import Cookies from "universal-cookie";
+import { CgProfile } from 'react-icons/cg';
+import { IconContext } from "react-icons";
+import imageOne from '../avatars/1.jpg'
+import imageTwo from '../avatars/2.jpg'
+import imageThree from '../avatars/3.jpg'
+import imageFour from '../avatars/4.jpg'
+import imageFive from '../avatars/5.jpg'
 
 const Card = () => {
   // const matches = [];
@@ -9,6 +18,14 @@ const Card = () => {
   const [currentIndex, setCurrentIndex] = useState(matches["data"].length - 1);
   const [lastDirection, setLastDirection] = useState();
   const cookies = new Cookies();
+
+  const avatarImages = [
+    imageOne,
+    imageTwo,
+    imageThree,
+    imageFour,
+    imageFive
+  ]
 
   // generate new people to swipe on
   const doServe = async () => {
@@ -56,8 +73,8 @@ const Card = () => {
       .then((data) => {
         if (data["success"]) {
           console.log(data["message"]);
-          
-          if(data["message"] === "") {
+
+          if (data["message"] === "") {
             // toast
           }
         } else console.log(data["message"]);
@@ -97,7 +114,7 @@ const Card = () => {
   const currentIndexRef = useRef(currentIndex);
 
   const childRefs = useMemo(
-    () => 
+    () =>
       Array(matches["data"].length)
         .fill(0)
         .map((i) => React.createRef()),
@@ -139,7 +156,7 @@ const Card = () => {
 
   return (
     <div className="fighterCard flex flex-col items-center h-screen">
-      <div className="cardContainer bg-gray">
+      <div className="cardContainer">
         {matches["data"].length === 0 ? (
           <div className="flex justify-center items-center text-white">
             Nobody wants that smoke
@@ -147,43 +164,51 @@ const Card = () => {
         ) : (
           matches["data"].length > 0 &&
           matches["data"].map((match, index) => (
-              <TinderCard
-                ref={childRefs[index]}
-                className="swipe"
-                key={index}
-                preventSwipe={["up", "down"]}
-                onSwipe={(dir) => swiped(dir, match._id, index)}
-                onCardLeftScreen={() => outOfFrame(match._id)}
-                flickOnSwipe={true}
+            <TinderCard
+              ref={childRefs[index]}
+              className="swipe rounded"
+              key={index}
+              preventSwipe={["up", "down"]}
+              onSwipe={(dir) => swiped(dir, match._id, index)}
+              onCardLeftScreen={() => outOfFrame(match._id)}
+              flickOnSwipe={true}
 
+            >
+              <div
+                style={{ backgroundColor: "#323232" }}
+                className="card rounded"
               >
-                <div
-                  // style={{ backgroundImage: "url(" + match.url + ")" }}
-                  style={{ backgroundColor: "grey" }}
-                  className="card"
-                >
-                  <div className="gridContainer">
-                    <div className="item1">
-                      {match["firstName"]} {match["lastName"]}
-                    </div>
-                    <div className="gridItem">Gender: {match["gender"]}</div>
-                    <div className="gridItem">
-                      Nickname: {match["nickname"]}
-                    </div>
-                    <div className="gridItem">Age: {match["age"]}</div>
-                    <div className="gridItem">Height: {match["height"]}</div>
-                    <div className="gridItem">Weight: {match["weight"]}</div>
-                    <div className="gridItem">Style: {match["style"]}</div>
-                    <div className="gridItem">Record: {match["record"]}</div>
-                    <div className="gridItem">Reach: {match["reach"]}</div>
+                <div className="gridContainer">
+                  <div className="item0">
+                      
+                    {match["image"] ? (<img
+                      style={{ height: "250px", width: "250px", borderRadius: "50%" }}
+                      src={avatarImages[match["image"]]} />) :
+                      (<IconContext.Provider value={{ color: "white", size: "250px", className: "avatar_icon" }}>
+                      <CgProfile />
+                    </IconContext.Provider>)}
                   </div>
+                  <div className="item1">
+                    {match["firstName"]} {match["lastName"]}
+                  </div>
+                  <div className="gridItem">Gender: {match["gender"]}</div>
+                  <div className="gridItem">
+                    Nickname: {match["nickname"]}
+                  </div>
+                  <div className="gridItem">Age: {match["age"]}</div>
+                  <div className="gridItem">Height: {match["height"]}</div>
+                  <div className="gridItem">Weight: {match["weight"]}</div>
+                  <div className="gridItem">Style: {match["style"]}</div>
+                  <div className="gridItem">Record: {match["record"]}</div>
+                  <div className="gridItem">Reach: {match["reach"]}</div>
                 </div>
-              </TinderCard>
+              </div>
+            </TinderCard>
           ))
-        )}  
+        )}
       </div>
-       <div className="buttons">
-         <button
+      <div className="buttons">
+        <button
           style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
           // onClick={() => swipe("left")}
           onClick={() => outOfFrame(match._id)}
@@ -204,7 +229,7 @@ const Card = () => {
         ) : (
           <h2 className="infoText">Swipe or get swiped on!</h2>
         )}
-       </div>
+      </div>
     </div>
   );
 };
